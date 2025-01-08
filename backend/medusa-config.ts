13a26f51968eaf56f4fe1e1904d752a495c277ee
NamSellsFish <nhatnam0192123@gmail.com>
2025-01-08 12:48:14 +0700

    initial commit

---
+++ backend/medusa-config.ts
@@ -0,0 +1,31 @@
+import { QUOTE_MODULE } from "./src/modules/quote";
+import { loadEnv, defineConfig, Modules } from "@medusajs/framework/utils";
+
+loadEnv(process.env.NODE_ENV!, process.cwd());
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+  },
+  modules: {
+    companyModuleService: {
+      resolve: "./modules/company",
+    },
+    [QUOTE_MODULE]: {
+      resolve: "./modules/quote",
+    },
+    [Modules.CACHE]: {
+      resolve: "@medusajs/medusa/cache-inmemory",
+    },
+    [Modules.WORKFLOW_ENGINE]: {
+      resolve: "@medusajs/medusa/workflow-engine-inmemory",
+    },
+  },
+});
---
+++ storefront/src/app/[countryCode]/(main)/account/layout.tsx
@@ -0,0 +1,14 @@
+import { retrieveCustomer } from "@lib/data/customer"
+import AccountLayout from "@modules/account/templates/account-layout"
+
+export default async function AccountPageLayout({
+  dashboard,
+  login,
+}: {
+  dashboard?: React.ReactNode
+  login?: React.ReactNode
+}) {
+  const customer = await retrieveCustomer().catch(() => null)
+
+  return <>{customer ? dashboard : login}</>
+}
